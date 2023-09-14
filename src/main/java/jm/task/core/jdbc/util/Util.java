93aa59b3938e7642105cb078183531d970623b9e
Rita M <kolotvinova03@bk.ru>
2023-09-14 20:09:05 +0300

    First commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    public static String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static String DB_URL = "jdbc:mysql://localhost:3306/database_name";
+    public static String DB_USERNAME = "root";
+    public static String DB_PASSWORD = "root";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            System.out.println("Connection OK!");
+        } catch (ClassNotFoundException | SQLException e ) {
+            e.printStackTrace();
+            System.out.println("Connection not OK(((9(");
+        }
+        return connection;
+    }
+
 }
